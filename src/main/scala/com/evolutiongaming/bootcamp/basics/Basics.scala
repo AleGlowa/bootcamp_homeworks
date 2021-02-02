1ea648a94888231d838421b71ee6b61f19eb2fcd
Alex Glowacki <klopsmonster@gmail.com>
2021-02-02 13:43:40 +0100

    basics.Basics - lcm now returns Option

--- src/main/scala/com/evolutiongaming/bootcamp/basics/Basics.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/Basics.scala
@@ -1,18 +1,19 @@
 package com.evolutiongaming.bootcamp.basics
 
+import scala.annotation.tailrec
+
 object Basics {
 
-  // `lcm` returns -1 for invalid arguments (a <= 0 or b <= 0)
-  def lcm(a: Int, b: Int): Int = if (a > 0 && b > 0) a * b / gcd(a, b) else -1
+  def lcm(a: Int, b: Int): Option[Int] = (a > 0, b > 0) match {
+    case (true, true) => Some(a * b / gcd(a, b))
+    case _ => None
+  }
 
+  @tailrec
   def gcd(a: Int, b: Int): Int = {
-    // case for two 0s
-    if (a == 0 && b == 0) Int.MaxValue
-    else {
-      // convert negative numbers to positive ones
-      val (aNonNeg, bNonNeg) = (Math.abs(a), Math.abs(b))
-      val (smaller, larger) = (aNonNeg min bNonNeg, aNonNeg max bNonNeg)
-      if (smaller == 0) larger else gcd(smaller, larger % smaller)
-    }
+    // convert negative numbers to positive ones
+    val (aNonNeg, bNonNeg) = (Math.abs(a), Math.abs(b))
+    val (smaller, larger) = (aNonNeg min bNonNeg, aNonNeg max bNonNeg)
+    if (smaller == 0) larger else gcd(smaller, larger % smaller)
   }
 }
