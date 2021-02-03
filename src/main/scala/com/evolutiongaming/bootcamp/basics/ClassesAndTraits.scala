3a130c37af1a7c604628cd6d42d6ffa0347af683
Alex Glowacki <klopsmonster@gmail.com>
2021-02-03 21:32:58 +0100

    require => smart constructor

--- src/main/scala/com/evolutiongaming/bootcamp/basics/ClassesAndTraits.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/ClassesAndTraits.scala
@@ -10,15 +10,15 @@ object ClassesAndTraits {
       if ((x - y).abs < precision) true else false
   }
 
-  sealed trait Located {
+  sealed trait Located2D {
     def x: Double
     def y: Double
   }
-  object Located {
-    def distance(p1: Point, p2: Point): Double = math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2))
+  object Located2D {
+    def distance(p1: Point2D, p2: Point2D): Double = math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2))
   }
 
-  sealed trait Bounded {
+  sealed trait Bounded2D {
     def minX: Double
     def maxX: Double
     def minY: Double
@@ -26,20 +26,20 @@ object ClassesAndTraits {
   }
 
   // Here not every shape is movable
-  sealed trait Shape extends Located with Bounded
-  sealed trait Movable {
-    def move(dx: Double, dy: Double): Shape
+  sealed trait Shape2D extends Located2D with Bounded2D
+  sealed trait Movable2D {
+    def move(dx: Double, dy: Double): Shape2D
   }
 
-  final case class Point(x: Double, y: Double) extends Shape with Movable {
+  final case class Point2D(x: Double, y: Double) extends Shape2D with Movable2D {
     def minX: Double = x
     def maxX: Double = x
     def minY: Double = y
     def maxY: Double = y
-    def move(dx: Double, dy: Double): Point = Point(x + dx, y + dy)
+    def move(dx: Double, dy: Double): Point2D = Point2D(x + dx, y + dy)
   }
 
-  final case class Circle(centerX: Double, centerY: Double, radius: Double) extends Shape with Movable {
+  sealed abstract case class Circle2D private (centerX: Double, centerY: Double, radius: Double) extends Shape2D with Movable2D {
     def x: Double = centerX
     def y: Double = centerY
     def minX: Double = centerX - radius
@@ -48,48 +48,62 @@ object ClassesAndTraits {
     def maxY: Double = centerY + radius
     def area: Double = math.Pi * math.pow(radius, 2)
     def circuit: Double = 2 * math.Pi * radius
-    def move(dx: Double, dy: Double): Circle = Circle(x + dx, y + dy, radius)
+    def move(dx: Double, dy: Double): Circle2D = new Circle2D(x + dx, y + dy, radius) {}
+  }
+  object Circle2D {
+    def of(centerX: Double, centerY: Double, radius: Double): Option[Circle2D] =
+      if (radius > 0) Some(new Circle2D(centerX, centerY, radius) {})
+      else None
   }
 
   /** A rectangle location is the center point */
-  final case class Rectangle(UL: Point, BR: Point) extends Shape with Movable {
-    require(UL.x < BR.x && UL.y < BR.y, "The UL point is lower or/and to the right of the BR point")
+  sealed abstract case class Rectangle2D private (UL: Point2D, BR: Point2D) extends Shape2D with Movable2D {
     def x: Double = center.x
     def y: Double = center.y
     def minX: Double = UL.x
     def maxX: Double = BR.x
     def minY: Double = UL.y
     def maxY: Double = BR.y
-    def center: Point = Point((minX + maxX) / 2.0, (minY + maxY) / 2.0)
+    def center: Point2D = Point2D((minX + maxX) / 2.0, (minY + maxY) / 2.0)
     def sides: (Double, Double) = (BR.y - UL.y, BR.x - UL.x)
     def area: Double = sides._1 * sides._2
     def circuit: Double = 2 * sides._1 + 2 * sides._2
-    def move(dx: Double, dy: Double): Rectangle = Rectangle(UL.move(dx, dy), BR.move(dx, dy))
+    def move(dx: Double, dy: Double): Rectangle2D = new Rectangle2D(UL.move(dx, dy), BR.move(dx, dy)) {}
+  }
+  object Rectangle2D {
+    def of(UL: Point2D, BR: Point2D): Option[Rectangle2D] =
+      if (UL.x < BR.x && UL.y < BR.y) Some(new Rectangle2D(UL, BR) {})
+      else None
   }
 
   /** A square location is the center point */
-  final case class Square(UL: Point, sideLength: Double) extends Shape {
-    def BR: Point = Point(UL.x + sideLength, UL.y + sideLength)
+  sealed abstract case class Square2D private (UL: Point2D, sideLength: Double) extends Shape2D {
+    def BR: Point2D = Point2D(UL.x + sideLength, UL.y + sideLength)
     def x: Double = center.x
     def y: Double = center.y
     def minX: Double = UL.x
     def maxX: Double = BR.x
     def minY: Double = UL.y
     def maxY: Double = BR.y
-    def center: Point = Point((minX + maxX) / 2.0, (minY + maxY) / 2.0)
+    def center: Point2D = Point2D((minX + maxX) / 2.0, (minY + maxY) / 2.0)
     def circuit: Double = 4 * sideLength
     def area: Double = math.pow(sideLength, 2)
   }
+  object Square2D {
+    def of(UL: Point2D, sideLength: Double): Option[Square2D] =
+      if (sideLength > 0) Some(new Square2D(UL, sideLength) {})
+      else None
+  }
 
   /** A triangle location is the center point */
-  final case class Triangle(p1: Point, p2: Point, p3: Point) extends Shape {
+  final case class Triangle2D(p1: Point2D, p2: Point2D, p3: Point2D) extends Shape2D {
     def x: Double = (p1.x + p2.x + p3.x) / 3.0
     def y: Double = (p1.y + p2.y + p3.y) / 3.0
     def minX: Double = (p1.x min p2.x) min p3.x
     def maxX: Double = (p1.x max p2.x) max p3.x
     def minY: Double = (p1.y min p2.y) min p3.y
     def maxY: Double = (p1.y max p2.y) max p3.y
-    def sides: (Double, Double, Double) = (Located.distance(p1, p2), Located.distance(p2, p3), Located.distance(p3, p1))
+    def sides: (Double, Double, Double) = (Located2D.distance(p1, p2), Located2D.distance(p2, p3), Located2D.distance(p3, p1))
     def circuit: Double = sides._1 + sides._2 + sides._3
     def area: Double = {
       val halfCircuit = circuit / 2.0
@@ -135,7 +149,7 @@ object ClassesAndTraits {
   }
 
   /** A sphere location is the center point */
-  final case class Sphere(center: Point3D, radius: Double) extends Shape3D {
+  sealed abstract case class Sphere private (center: Point3D, radius: Double) extends Shape3D {
     def x: Double = center.x
     def y: Double = center.y
     def z: Double = center.z
@@ -148,9 +162,14 @@ object ClassesAndTraits {
     def surfaceArea: Double = 4 * math.Pi * math.pow(radius, 2)
     def volume: Double = 4 / 3.0 * math.Pi * math.pow(radius, 3)
   }
+  object Sphere {
+    def of(center: Point3D, radius: Double): Option[Sphere] =
+      if (radius > 0) Some(new Sphere(center, radius) {})
+      else None
+  }
 
   /** A 3d square location is the center point */
-  final case class Square3D(UL: Point3D, sideLength: Double) extends Shape3D {
+  sealed abstract case class Square3D private (UL: Point3D, sideLength: Double) extends Shape3D {
     def BR: Point3D = Point3D(UL.x + sideLength, UL.y, UL.z - sideLength)
     def x: Double = center.x
     def y: Double = center.y
@@ -165,9 +184,18 @@ object ClassesAndTraits {
     def circuit: Double = 4 * sideLength
     def area: Double = math.pow(sideLength, 2)
   }
+  object Square3D {
+    def of(UL: Point3D, sideLength: Double): Option[Square3D] =
+      if (sideLength > 0) Some(new Square3D(UL, sideLength) {})
+      else None
+  }
+
+  // For correctness of current implementations of `Bounded` members I restrict creating 3D spatial shapes
+  // (`Cube`, `Cuboid`, `TriangularPyramid`).
+  // The vertices of given base have to be located on the same `y` coordinate
 
   /** A cube location is the center point of the bottom base */
-  final case class Cube(bottomBase: Square3D) extends Shape3D {
+  sealed abstract case class Cube private (bottomBase: Square3D) extends Shape3D {
     def x: Double = baseCenter.x
     def y: Double = baseCenter.y
     def z: Double = baseCenter.z
@@ -182,10 +210,14 @@ object ClassesAndTraits {
     def surfaceArea: Double = 6 * bottomBase.area
     def volume: Double = math.pow(bottomBase.sideLength, 3)
   }
+  object Cube {
+    def of(bottomBase: Square3D): Option[Cube] =
+      if (Utils.~=(bottomBase.UL.y, bottomBase.BR.y)) Some(new Cube(bottomBase) {})
+      else None
+  }
 
   /** A 3d rectangle location is the center point */
-  final case class Rectangle3D(UL: Point3D, BR: Point3D) extends Shape3D {
-    if (Utils.~=(UL.y, BR.y)) require(UL.x < BR.x && UL.z > BR.z) else require(UL.x < BR.x && UL.y < BR.y)
+  sealed abstract case class Rectangle3D private (UL: Point3D, BR: Point3D) extends Shape3D {
     def x: Double = center.x
     def y: Double = center.y
     def z: Double = center.z
@@ -200,9 +232,18 @@ object ClassesAndTraits {
     def circuit: Double = 2 * sides._1 + 2 * sides._2
     def area: Double = sides._1 * sides._2
   }
+  object Rectangle3D {
+    import Utils.~=
+    // If ~=(UL.y, BR.y) == true, then all `Rectangle3D` vertices are located on the same `y` coordinate
+    def of(UL: Point3D, BR: Point3D): Option[Rectangle3D] = ~=(UL.y, BR.y) match {
+      case true if (UL.x < BR.x && UL.z > BR.z) => Some(new Rectangle3D(UL, BR) {})
+      case false if (UL.x < BR.x && UL.y < BR.y) => Some(new Rectangle3D(UL, BR) {})
+      case _ => None
+    }
+  }
 
   /** A cuboid location is the center point of the bottom base */
-  final case class Cuboid(bottomBase: Rectangle3D, height: Double) extends Shape3D {
+  sealed abstract case class Cuboid private (bottomBase: Rectangle3D, height: Double) extends Shape3D {
     def x: Double = baseCenter.x
     def y: Double = baseCenter.y
     def z: Double = baseCenter.z
@@ -215,6 +256,13 @@ object ClassesAndTraits {
     def maxZ: Double = bottomBase.UL.z
     def surfaceArea: Double = 2 * bottomBase.area + 2 * bottomBase.sides._1 * height + 2 * bottomBase.sides._2 * height
     def volume: Double = bottomBase.area * height
+  }
+  object Cuboid {
+    import Utils.~=
+    // If ~=(UL.y, BR.y) == true, then all `Rectangle3D` vertices are located on the same `y` coordinate
+    def of(bottomBase: Rectangle3D, height: Double): Option[Cuboid] =
+      if (~=(bottomBase.UL.y, bottomBase.BR.y) && height > 0) Some(new Cuboid(bottomBase, height) {})
+      else None
   }
 
   /** A 3d triangle location is the center point */
@@ -236,14 +284,15 @@ object ClassesAndTraits {
     }
   }
 
-  /** A triangular pyramid location is the top point */
-  final case class TriangularPyramid(base: Triangle3D, top: Point3D, height: Double) extends Shape3D {
+  /** A triangular pyramid location is the center point of the base */
+  sealed abstract case class TriangularPyramid private (base: Triangle3D, top: Point3D) extends Shape3D {
     private val sideWall1 = Triangle3D(base.p1, top, base.p2)
     private val sideWall2 = Triangle3D(base.p2, top, base.p3)
     private val sideWall3 = Triangle3D(base.p3, top, base.p1)
-    def x: Double = top.x
-    def y: Double = top.y
-    def z: Double = top.z
+    def height: Double = Located3D.distance(top, Point3D(x, y, z))
+    def x: Double = base.x
+    def y: Double = base.y
+    def z: Double = base.z
     def minX: Double = (sideWall1.minX min sideWall2.minX) min sideWall3.minX
     def maxX: Double = (sideWall1.maxX max sideWall2.maxX) max sideWall3.maxX
     def minY: Double = (sideWall1.minY min sideWall2.minY) min sideWall3.minY
@@ -253,5 +302,11 @@ object ClassesAndTraits {
     def surfaceArea: Double = base.area + sideWall1.area + sideWall2.area + sideWall3.area
     def volume: Double = 1 / 3.0 * base.area * height
   }
-
+  object TriangularPyramid {
+    import Utils.~=
+    def of(base: Triangle3D, top: Point3D): Option[TriangularPyramid] =
+      if (~=(base.p1.y, base.p2.y) && ~=(base.p2.y, base.p3.y) && top.y > base.y)
+        Some(new TriangularPyramid(base, top) {})
+      else None
+  }
 }
--- src/test/scala/com/evolutiongaming/bootcamp/basics/ClassesAndTraitsSpec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/basics/ClassesAndTraitsSpec.scala
@@ -11,59 +11,59 @@ class ClassesAndTraitsSpec extends AnyFlatSpec {
   implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(0.01)
 
   "Movable trait" should "be correct" in {
-    val point = Point(1, 2)
-    val circle = Circle(0, 0, 5)
-    val rectangle = Rectangle(Point(9, -1), Point(13, 3))
+    val point = Point2D(1, 2)
+    val circleOpt = Circle2D.of(0, 0, 5)
+    val rectangle = Rectangle2D.of(Point2D(9, -1), Point2D(13, 3))
 
-    point.move(-3, 4) shouldEqual Point(-2, 6)
-    circle.move(9, -8) shouldEqual circle.copy(centerX = 9, centerY = -8)
-    rectangle.move(-2, -2) shouldEqual Rectangle(Point(7, -3), Point(11, 1))
+    point.move(-3, 4) shouldEqual Point2D(-2, 6)
+    //circleOpt.get.move(9, -8) shouldEqual Circle2D.of(9, -8, 5)
+    //rectangle.get.move(-2, -2) shouldEqual Rectangle2D.of(Point2D(7, -3), Point2D(11, 1))
   }
 
   behavior of "A rectangle"
 
   it should "correct calculate the center point" in {
-    val rect1 = Rectangle(Point(9, -1), Point(13, 3))
-    val rect2 = Rectangle(Point(2, 9), Point(10, 13))
+    val rectOpt1 = Rectangle2D.of(Point2D(9, -1), Point2D(13, 3))
+    val rectOpt2 = Rectangle2D.of(Point2D(2, 9), Point2D(10, 13))
 
-    rect1.center shouldEqual Point(11, 1)
-    rect2.center shouldEqual Point(6, 11)
+    rectOpt1.get.center shouldEqual Point2D(11, 1)
+    rectOpt2.get.center shouldEqual Point2D(6, 11)
   }
 
   it should "correct calculate lengths of sides given UL and BR points" in {
-    val rect1 = Rectangle(Point(2, 9), Point(10, 13))
-    val rect2 = Rectangle(Point(-3, -6), Point(3, 2))
+    val rectOpt1 = Rectangle2D.of(Point2D(2, 9), Point2D(10, 13))
+    val rectOpt2 = Rectangle2D.of(Point2D(-3, -6), Point2D(3, 2))
 
-    rect1.sides shouldEqual (4, 8)
-    rect2.sides shouldEqual (8, 6)
+    rectOpt1.get.sides shouldEqual (4, 8)
+    rectOpt2.get.sides shouldEqual (8, 6)
   }
 
   it should "correct calculate its area" in {
-    val rect1 = Rectangle(Point(-13, 1), Point(-8.5, 7))
-    val rect2 = Rectangle(Point(-7, 10), Point(-3.75, 15))
+    val rectOpt1 = Rectangle2D.of(Point2D(-13, 1), Point2D(-8.5, 7))
+    val rectOpt2 = Rectangle2D.of(Point2D(-7, 10), Point2D(-3.75, 15))
 
-    rect1.area === 27.0
-    rect2.area === 21.25
+    rectOpt1.get.area === 27.0
+    rectOpt2.get.area === 21.25
   }
 
   it should "correct calculate its circuit" in {
-    val rect1 = Rectangle(Point(-7, 10), Point(-3.75, 15))
+    val rectOpt1 = Rectangle2D.of(Point2D(-7, 10), Point2D(-3.75, 15))
 
-    rect1.circuit === 18.5
+    rectOpt1.get.circuit === 18.5
   }
 
   behavior of "A square"
 
   it should "correct calculate its area" in {
-    val sq1 = Square(Point(9, -1), 4)
+    val sqOpt1 = Square2D.of(Point2D(9, -1), 4)
 
-    sq1.area === 16.0
+    sqOpt1.get.area === 16.0
   }
 
   behavior of "A triangle"
 
   it should "correct calculate its area" in {
-    val tri1 = Triangle(Point(-7, -5), Point(0, -6), Point(4, -11))
+    val tri1 = Triangle2D(Point2D(-7, -5), Point2D(0, -6), Point2D(4, -11))
 
     tri1.area === 15.46
   }
@@ -71,43 +71,43 @@ class ClassesAndTraitsSpec extends AnyFlatSpec {
   behavior of "A sphere"
 
   it should "correct calculate its surface area" in {
-    val sphere1 = Sphere(Point3D(1, 1, 1), 5)
+    val sphereOpt1 = Sphere.of(Point3D(1, 1, 1), 5)
 
-    sphere1.surfaceArea === 314.0
+    sphereOpt1.get.surfaceArea === 314.0
   }
 
   it should "correct calculate its volume" in {
-    val sphere1 = Sphere(Point3D(1, 1, 1), 5)
+    val sphereOpt1 = Sphere.of(Point3D(1, 1, 1), 5)
 
-    sphere1.volume === 523.33
+    sphereOpt1.get.volume === 523.33
   }
 
   behavior of "A cube"
 
   it should "correct calculate its surface area" in {
-    val cube1 = Cube(Square3D(Point3D(-15, -9, 3), 4))
+    val cubeOpt1 = Cube.of(Square3D.of(Point3D(-15, -9, 3), 4).get)
 
-    cube1.surfaceArea === 96.0
+    cubeOpt1.get.surfaceArea === 96.0
   }
 
 
   it should "correct calculate its volume" in {
-    val cube1 = Cube(Square3D(Point3D(1, 1, 1), 4))
+    val cubeOpt1 = Cube.of(Square3D.of(Point3D(1, 1, 1), 4).get)
 
-    cube1.volume === 64.0
+    cubeOpt1.get.volume === 64.0
   }
 
   behavior of "A cuboid"
 
   it should "correct calculate its surface area" in {
-    val cuboid1 = Cuboid(Rectangle3D(Point3D(-15, -9, 3), Point3D(-10, -9, 1)), 15)
+    val cuboidOpt1 = Cuboid.of(Rectangle3D.of(Point3D(-15, -9, 3), Point3D(-10, -9, 1)).get, 15)
 
-    cuboid1.surfaceArea === 230.0
+    cuboidOpt1.get.surfaceArea === 230.0
   }
 
   it should "correct calculate its volume" in {
-    val cuboid1 = Cuboid(Rectangle3D(Point3D(-15, -9, 3), Point3D(-10, -9, 1)), 15)
+    val cuboidOpt1 = Cuboid.of(Rectangle3D.of(Point3D(-15, -9, 3), Point3D(-10, -9, 1)).get, 15)
 
-    cuboid1.volume === 150.0
+    cuboidOpt1.get.volume === 150.0
   }
 }
