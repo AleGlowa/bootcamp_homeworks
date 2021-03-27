d4680c42788ec894e9f7bcf4b7ff6c9202069e65
Alex Glowacki <klopsmonster@gmail.com>
2021-03-27 19:41:20 +0100

    testing2.Calculator - done

--- src/main/scala/com/evolutiongaming/bootcamp/testing2/Calculator.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/testing2/Calculator.scala
@@ -1,3 +1,148 @@
 package com.evolutiongaming.bootcamp.testing2
 
-case class Calculator()
+import com.evolutiongaming.bootcamp.testing2.Calculator._
+
+import java.math.{MathContext, RoundingMode}
+import scala.util.Try
+
+/** Simple calculator with buttons.
+ *
+ * @param memory whatever is stored in the memory.
+ * @param screen whatever you see on the screen.
+ */
+final case class Calculator(memory: BigDecimal = 0d, screen: Screen = Screen.of("0").get, operation: Option[Operation] = None) {
+
+  def restart: Calculator = Calculator()
+
+  def enterDigit(digit: Input.Digit): Calculator =
+    this.copy(screen = Screen.of(s"${screen.toString}${digit.data}").get)
+
+  def enterDecPoint: Calculator =
+    if (screen.strNumber.last == '.')
+      this
+    else
+      this.copy(screen = Screen.of(s"${screen.toString}${Input.DecPoint.data}").get)
+
+  def plus: Calculator = {
+    val mem = screen.optConvertToNumWithPoint
+
+    Calculator(operation = Some(Operation.Plus), screen = Screen.of("0").get,
+      memory = mem)
+  }
+
+  def minus: Calculator = {
+    val mem = screen.optConvertToNumWithPoint
+
+    Calculator(operation = Some(Operation.Minus), screen = Screen.of("0").get,
+      memory = mem)
+  }
+
+  def multiply: Calculator = {
+    val mem = screen.optConvertToNumWithPoint
+
+    Calculator(operation = Some(Operation.Multiply), screen = Screen.of("0").get,
+      memory = mem)
+  }
+
+  def divide: Calculator = {
+    val mem = screen.optConvertToNumWithPoint
+
+    Calculator(operation = Some(Operation.Divide), screen = Screen.of("0").get,
+      memory = mem)
+  }
+
+  def calculate: Either[String, Calculator] = operation.fold[Either[String, Calculator]](Right(this)) {
+
+    case Operation.Plus => for {
+      res <- Try(memory + screen.getNumber).toEither.left.map(_ => "ERR - overflow after plus")
+    } yield Calculator(screen = Screen.of(res.toString).get)
+
+    case Operation.Minus =>
+      val res = memory - screen.getNumber
+      Right(Calculator(screen = Screen.of(res.toString).get))
+
+    case Operation.Multiply => for {
+      res <- Try(memory * screen.getNumber).toEither.left.map(_ => "ERR - overflow after multiply")
+    } yield Calculator(screen = Screen.of(res.toString).get)
+
+    case Operation.Divide => for {
+      res <- Try((memory / screen.getNumber).setScale(10, BigDecimal.RoundingMode.HALF_UP)).toEither.left.map(_ => "ERR - division by 0")
+    } yield Calculator(screen = Screen.of(res.toString).get)
+  }
+}
+object Calculator {
+
+  sealed trait Input[+A] {
+    def data: A
+    override def toString: String = data.toString
+  }
+  object Input {
+
+    sealed trait Digit extends Input[Int]
+    object Digit {
+      case object Zero extends Digit {
+        override def data: Int = 0
+      }
+      case object One extends Digit {
+        override def data: Int = 1
+      }
+      case object Two extends Digit {
+        override def data: Int = 2
+      }
+      case object Three extends Digit {
+        override def data: Int = 3
+      }
+      case object Four extends Digit {
+        override def data: Int = 4
+      }
+      case object Five extends Digit {
+        override def data: Int = 5
+      }
+      case object Six extends Digit {
+        override def data: Int = 6
+      }
+      case object Seven extends Digit {
+        override def data: Int = 7
+      }
+      case object Eight extends Digit {
+        override def data: Int = 8
+      }
+      case object Nine extends Digit {
+        override def data: Int = 9
+      }
+    }
+
+    object DecPoint extends Input[Char] {
+      override def data: Char = '.'
+    }
+  }
+
+  sealed abstract case class Screen private (strNumber: String) {
+
+    override def toString: String = if (strNumber.length > 1) strNumber.dropWhile(_ == '0') else strNumber
+
+    def getNumber: BigDecimal = BigDecimal(strNumber, Screen.mc)
+
+    def optConvertToNumWithPoint: BigDecimal =
+      if (strNumber.last == '.')
+        BigDecimal(s"$getNumber.0", Screen.mc)
+      else getNumber
+  }
+  object Screen {
+    val ScreenLength: Int = 10
+    private val mc = new MathContext(ScreenLength, RoundingMode.UNNECESSARY)
+
+    def of(strNumber: String): Option[Screen] =
+      if (strNumber.count(_ != '.') <= ScreenLength)
+        Try(BigDecimal(strNumber, mc)).toOption.map(_ => new Screen(strNumber) {})
+      else None
+  }
+
+  sealed trait Operation
+  object Operation {
+    case object Plus     extends Operation
+    case object Minus    extends Operation
+    case object Multiply extends Operation
+    case object Divide   extends Operation
+  }
+}
--- src/test/scala/com/evolutiongaming/bootcamp/testing2/CalculatorSpec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/testing2/CalculatorSpec.scala
@@ -1,5 +1,195 @@
 package com.evolutiongaming.bootcamp.testing2
 
-class CalculatorSpec {
+import com.evolutiongaming.bootcamp.testing2.Calculator.Input
+import com.evolutiongaming.bootcamp.testing2.Calculator.Input.Digit._
+import com.evolutiongaming.bootcamp.testing2.Calculator.Input._
+import com.evolutiongaming.bootcamp.testing2.Calculator.Screen.ScreenLength
+import org.scalacheck.Gen
+import org.scalatest.{EitherValues, OptionValues}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
+import java.math.{MathContext, RoundingMode}
+import scala.util.Random
+
+trait CalculatorUtils {
+  import Calculator.Screen
+
+  val starterCalc = Calculator()
+  val calcWithPoint = Calculator(screen = Screen.of("0.").get)
+  val mc = new MathContext(ScreenLength, RoundingMode.UNNECESSARY)
+
+  def enterNumber[A](calc: Calculator, number: List[Input[A]]): Calculator =
+    number.foldLeft(calc) {
+      case (c: Calculator, d: Digit) => c.enterDigit(d)
+      case (c, _)                    => c.enterDecPoint
+    }
+}
+
+// @nowarn
+class CalculatorSpec
+  extends AnyWordSpec
+    with Matchers
+    with OptionValues
+    with EitherValues
+    with ScalaCheckDrivenPropertyChecks
+    with CalculatorUtils {
+
+  // Generators
+  val digitGen = Gen.oneOf(Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine)
+
+  val numbersWithoutPointGen = for {
+    len    <- Gen.choose(1, 5)
+    number <- Gen.listOfN(len, digitGen)
+    if number.head != Digit.Zero
+  } yield number
+
+  val numbersWithPointGen = for {
+    len         <- Gen.choose(2, 6)
+    lenBefore   = Random.between(1, len)
+    beforePoint <- Gen.listOfN(lenBefore, digitGen)
+    if beforePoint.head != Digit.Zero
+    lenAfter    = len - lenBefore
+    afterPoint  <- Gen.listOfN(lenAfter, digitGen)
+  } yield beforePoint ::: DecPoint :: afterPoint
+
+  val numbersGen = Gen.oneOf(numbersWithoutPointGen, numbersWithPointGen)
+
+  // Tests
+  "Calculator" should {
+    "enter the numbers correctly" in {
+      forAll(numbersGen) { number =>
+        val screen = enterNumber(starterCalc, number).screen
+
+        screen.toString shouldEqual number.mkString
+      }
+    }
+
+    "does nothing" when {
+      "you just repeat pressing `=`" in {
+        for {
+          calc1 <- starterCalc.calculate
+          calc2 <- calc1.calculate
+          calc3 <- calc2.calculate
+          calc4 <- calc3.calculate
+        } yield calc4 shouldEqual starterCalc
+      }
+    }
+
+    "calculate sum correctly" when {
+      "operands are integers" in {
+        forAll(numbersWithoutPointGen, numbersWithoutPointGen) { (number1, number2) =>
+          val calcAfterFirstNumber  = enterNumber(starterCalc, number1)
+          val calcAfterPlus         = calcAfterFirstNumber.plus
+          val calcAfterSecondNumber = enterNumber(calcAfterPlus, number2)
+
+          val (num1, num2) = (BigDecimal(number1.mkString, mc), BigDecimal(number2.mkString, mc))
+          calcAfterSecondNumber.calculate.right.value.screen.getNumber shouldEqual (num1 + num2)
+        }
+      }
+      "operands are floats" in {
+        forAll(numbersWithPointGen, numbersWithPointGen) { (number1, number2) =>
+          val calcAfterFirstNumber  = enterNumber(starterCalc, number1)
+          val calcAfterPlus         = calcAfterFirstNumber.plus
+          val calcAfterSecondNumber = enterNumber(calcAfterPlus, number2)
+
+          val (num1, num2) = (BigDecimal(number1.mkString, mc), BigDecimal(number2.mkString, mc))
+          calcAfterSecondNumber.calculate.right.value.screen.getNumber shouldEqual (num1 + num2)
+        }
+      }
+    }
+
+    "calculate difference correctly" when {
+      "operands are integers" in {
+        forAll(numbersWithoutPointGen, numbersWithoutPointGen) { (number1, number2) =>
+          val calcAfterFirstNumber  = enterNumber(starterCalc, number1)
+          val calcAfterMinus        = calcAfterFirstNumber.minus
+          val calcAfterSecondNumber = enterNumber(calcAfterMinus, number2)
+
+          val (num1, num2) = (BigDecimal(number1.mkString, mc), BigDecimal(number2.mkString, mc))
+          calcAfterSecondNumber.calculate.right.value.screen.getNumber shouldEqual (num1 - num2)
+        }
+      }
+      "operands are floats" in {
+        forAll(numbersWithPointGen, numbersWithPointGen) { (number1, number2) =>
+          val calcAfterFirstNumber  = enterNumber(starterCalc, number1)
+          val calcAfterMinus        = calcAfterFirstNumber.minus
+          val calcAfterSecondNumber = enterNumber(calcAfterMinus, number2)
+
+          val (num1, num2) = (BigDecimal(number1.mkString, mc), BigDecimal(number2.mkString, mc))
+          calcAfterSecondNumber.calculate.right.value.screen.getNumber shouldEqual (num1 - num2)
+        }
+      }
+    }
+
+    "calculate product correctly" when {
+      "operands are integers" in {
+        forAll(numbersWithoutPointGen, numbersWithoutPointGen) { (number1, number2) =>
+          val calcAfterFirstNumber     = enterNumber(starterCalc, number1)
+          val calcAfterMultiply        = calcAfterFirstNumber.multiply
+          val calcAfterSecondNumber    = enterNumber(calcAfterMultiply, number2)
+
+          val (num1, num2) = (BigDecimal(number1.mkString, mc), BigDecimal(number2.mkString, mc))
+          calcAfterSecondNumber.calculate.right.value.screen.getNumber shouldEqual (num1 * num2)
+        }
+      }
+      "operands are floats" in {
+        forAll(numbersWithPointGen, numbersWithPointGen) { (number1, number2) =>
+          val calcAfterFirstNumber     = enterNumber(starterCalc, number1)
+          val calcAfterMultiply        = calcAfterFirstNumber.multiply
+          val calcAfterSecondNumber    = enterNumber(calcAfterMultiply, number2)
+
+          val (num1, num2) = (BigDecimal(number1.mkString, mc), BigDecimal(number2.mkString, mc))
+          calcAfterSecondNumber.calculate.right.value.screen.getNumber shouldEqual (num1 * num2)
+        }
+      }
+    }
+
+    "print `ERR - division by 0` string when trying to divide by 0" in {
+      forAll(numbersGen) { number =>
+        val calcAfterNumber = enterNumber(starterCalc, number)
+        val calcAfterOp     = calcAfterNumber.divide
+        val calcAfterZero   = calcAfterOp.enterDigit(Zero)
+
+        calcAfterZero.calculate.left.value shouldEqual "ERR - division by 0"
+      }
+    }
+
+    "displays 0" when {
+      "you are pressing same 0s at the beginning" in {
+        val calc1 = starterCalc.enterDigit(Zero)
+        val calc2 = calc1.enterDigit(Zero)
+        val calc3 = calc2.enterDigit(Zero)
+        val calc4 = calc3.enterDigit(Zero)
+
+        calc4.screen.toString shouldEqual starterCalc.screen.toString
+      }
+    }
+
+    "calculate correctly chain of calculations" in {
+      // 2.356 + 1000.9 - 6 * 423.23 / 10 = 42206.865
+      val number1 = List(Two, DecPoint, Three, Five, Six)
+      val number2 = List(One, Zero, Zero, Zero, DecPoint, Nine)
+      val number3 = List(Six)
+      val number4 = List(Four, Two, Three, DecPoint, Two, Three)
+      val number5 = List(One, Zero)
+
+      val calc1 = enterNumber(starterCalc, number1)
+      val calc2 = calc1.plus
+      val calc3 = enterNumber(calc2, number2)
+      for {
+        calc4 <- calc3.calculate
+        calc5   = calc4.minus
+        calc6   = enterNumber(calc5, number3)
+        calc7  <- calc6.calculate
+        calc8   = calc7.multiply
+        calc9   = enterNumber(calc8, number4)
+        calc10 <- calc9.calculate
+        calc11  = calc10.divide
+        calc12  = enterNumber(calc11, number5)
+        calc13 <- calc12.calculate
+      } yield calc13.screen.getNumber shouldEqual BigDecimal(42206.865, mc)
+    }
+  }
 }
